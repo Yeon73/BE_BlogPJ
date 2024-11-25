ab4e63af5edd9182144cc6efcaee4f6f91d5de53
Yeon73 <yunalove7373@gmail.com>
2024-11-26 00:23:41 +0900

    Feat: Implement API for Modifying Blog Article
    Domain, DTO, Service, Controller method, test code

--- springboot-developer/src/main/java/me/byeongyeonkim/springbootdeveloper/controller/BlogApiController.java
+++ springboot-developer/src/main/java/me/byeongyeonkim/springbootdeveloper/controller/BlogApiController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import me.byeongyeonkim.springbootdeveloper.domain.Article;
 import me.byeongyeonkim.springbootdeveloper.dto.AddArticleRequest;
 import me.byeongyeonkim.springbootdeveloper.dto.ArticleResponse;
+import me.byeongyeonkim.springbootdeveloper.dto.UpdateArticleRequest;
 import me.byeongyeonkim.springbootdeveloper.service.BlogService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -54,5 +55,14 @@ public class BlogApiController {
 
         return ResponseEntity.ok()
                 .build();
+    }
+
+    @PutMapping("/api/articles/{id}")
+    public ResponseEntity<Article> updateArticle(@PathVariable long id,
+                                                 @RequestBody UpdateArticleRequest request) {
+        Article updatedArticle = blogService.update(id, request);
+
+        return ResponseEntity.ok()
+                .body(updatedArticle);
     }
 }
--- springboot-developer/src/test/java/me/byeongyeonkim/springbootdeveloper/controller/BlogApiControllerTest.java
+++ springboot-developer/src/test/java/me/byeongyeonkim/springbootdeveloper/controller/BlogApiControllerTest.java
@@ -3,6 +3,7 @@ package me.byeongyeonkim.springbootdeveloper.controller;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import me.byeongyeonkim.springbootdeveloper.domain.Article;
 import me.byeongyeonkim.springbootdeveloper.dto.AddArticleRequest;
+import me.byeongyeonkim.springbootdeveloper.dto.UpdateArticleRequest;
 import me.byeongyeonkim.springbootdeveloper.repository.BlogRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -144,5 +145,37 @@ class BlogApiControllerTest {
         List<Article> articles = blogRepository.findAll();
 
         assertThat(articles).isEmpty();
+    }
+
+    @DisplayName("updateArticle: 블로그 글 수정에 성공한다.")
+    @Test
+    public void updateArticle() throws Exception {
+        // given
+        final String url = "/api/articles/{id}";
+        final String title = "title";
+        final String content = "content";
+
+        Article savedArticle = blogRepository.save(Article.builder()
+                .title(title)
+                .content(content)
+                .build());
+
+        final String newTitle = "new title";
+        final String newContent = "new content";
+
+        UpdateArticleRequest request = new UpdateArticleRequest(newTitle, newContent);
+
+        // when
+        ResultActions result = mockMvc.perform(put(url, savedArticle.getId())
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .content(objectMapper.writeValueAsString(request)));
+
+        // then
+        result.andExpect(status().isOk());
+
+        Article article = blogRepository.findById(savedArticle.getId()).get();
+
+        assertThat(article.getTitle()).isEqualTo(newTitle);
+        assertThat(article.getContent()).isEqualTo(newContent);
     }
 }

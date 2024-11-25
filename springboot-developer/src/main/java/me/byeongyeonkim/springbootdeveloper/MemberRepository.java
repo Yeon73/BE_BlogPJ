14148bb44792d3cfc5b004c3d8056e72ad6209fc
Yeon73 <yunalove7373@gmail.com>
2024-11-25 22:34:08 +0900

    Remove: Delete Test Code
    All the test code for studying, starting new project

--- springboot-developer/src/main/java/me/byeongyeonkim/springbootdeveloper/MemberRepository.java
+++
@@ -1,6 +0,0 @@
-package me.byeongyeonkim.springbootdeveloper;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberRepository extends JpaRepository<Member, Long> {
-}

3091c68742cad941f47e0030d78ea7bc55122248
Yeon73 <yunalove7373@gmail.com>
2024-11-25 23:46:57 +0900

    Feat: Implement API for Blog Article List Inquiry
    Service, Controller method, SQL test code

---
+++ springboot-developer/src/main/java/me/byeongyeonkim/springbootdeveloper/dto/ArticleResponse.java
@@ -0,0 +1,15 @@
+package me.byeongyeonkim.springbootdeveloper.dto;
+
+import lombok.Getter;
+import me.byeongyeonkim.springbootdeveloper.domain.Article;
+
+@Getter
+public class ArticleResponse {
+    private final String title;
+    private final String content;
+
+    public ArticleResponse(Article article) {
+        this.title = article.getTitle();
+        this.content = article.getContent();
+    }
+}

3c414f56b2c5c872dbfa2f32bc6b24d8a61b2612
Yeon73 <yunalove7373@gmail.com>
2024-11-28 11:42:11 +0900

    Feat: Implement Login/out using JWT Authentication
    JWT Provider, Refresh token, Token filter, Token API

---
+++ springboot-developer/src/main/java/me/byeongyeonkim/springbootdeveloper/service/RefreshTokenService.java
@@ -0,0 +1,17 @@
+package me.byeongyeonkim.springbootdeveloper.service;
+
+import lombok.RequiredArgsConstructor;
+import me.byeongyeonkim.springbootdeveloper.domain.RefreshToken;
+import me.byeongyeonkim.springbootdeveloper.repository.RefreshTokenRepository;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class RefreshTokenService {
+    private final RefreshTokenRepository refreshTokenRepository;
+
+    public RefreshToken findByRefreshToken(String refreshToken) {
+        return refreshTokenRepository.findByRefreshToken(refreshToken)
+                .orElseThrow(() -> new IllegalArgumentException("Unexpected token"));
+    }
+}
